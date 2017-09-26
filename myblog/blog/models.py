11908557f55f76c3d77cebe4c79e6741afce7005
jack <1169386855@qq.com>
2017-09-26 18:12:57 +0800

    models urls

--- myblog/blog/models.py
+++ myblog/blog/models.py
@@ -1,3 +1,31 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class Category(models.Model):
+	name = models.CharField(max_length = 100)
+
+	def __str__(self):
+		return self.name
+
+
+class Tag(models.Model):
+	name = models.CharField(max_length=100)
+
+	def __str__(self):
+		return self.name
+
+
+
+class Post(models.Model):
+	title = models.CharField(max_length=200)
+	body = models.TextField()
+	create_time = models.DateTimeField()
+	modified_time = models.DateTimeField()
+	category = models.ForeignKey(Category)
+	tags = models.ManyToManyField(Tag)
+	author = models.ForeignKey(User)
+
+	def __str__(self):
+		return self.name
