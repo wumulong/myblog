fd17f88308b21fd383cc77b9ad66bfa42a8fd809
jack <1169386855@qq.com>
2017-10-04 21:56:45 +0800

    add

---
+++ myblog/blog/templatetags/blog_tags.py
@@ -0,0 +1,21 @@
+from django import template
+from ..models import Post, Category
+
+
+register = template.Library()
+
+
+
+@register.simple_tag
+def get_recent_posts(num=5):
+	return Post.objects.all().order_by('-create_time')[:num]
+
+
+@register.simple_tag
+def archives():
+	return Post.objects.dates('create_time', 'month', order='DESC')
+
+
+@register.simple_tag
+def get_categorys():
+	return Category.objects.all()
